8c0b5a9e225a2806421db276fe7b2c71988e4931
AnaG0307 <ana.genover@gmail.com>
2021-12-01 21:06:41 +0000

    Updated urls env.py

--- myrecipebooka/settings.py
+++ myrecipebooka/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["my-recipe-book-project.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["my-recipe-book-a.herokuapp.com", "localhost"]
 
 
 # Application definition
