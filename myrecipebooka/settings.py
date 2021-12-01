0ac1672dfb6398b43d71321fc42dadb90eb2d8c5
AnaG0307 <ana.genover@gmail.com>
2021-12-01 15:45:07 +0000

    Updated DATABASE_URL from Heroku

--- myrecipebooka/settings.py
+++ myrecipebooka/settings.py
@@ -90,8 +90,7 @@ WSGI_APPLICATION = 'myrecipebooka.wsgi.application'
 # }
 
 DATABASES = {
-    'default':
-    dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
