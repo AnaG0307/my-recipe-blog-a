f78a5e0a73bf09aca87f4066bef6a6dc426a4d8d
AnaG0307 <ana.genover@gmail.com>
2021-12-03 18:03:26 +0000

    Added admin approval options for user comments and user recipes

--- recipe_blog/admin.py
+++ recipe_blog/admin.py
@@ -5,13 +5,26 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-
-    search_fields = ['author', 'title', 'content', 'created_on', 'approved', 'featured']
+    search_fields = [
+        'author',
+        'title',
+        'content',
+        'created_on',
+        'approved',
+        'featured'
+        ]
     list_display = ('author', 'title', 'created_on', 'approved', 'featured')
     date_hierarchy = 'created_on'
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('approved', 'created_on', 'featured')
     summernote_fields = ('content', 'excerpt')
+    actions = ['user_recipe_public', 'feature_recipe']
+
+    def user_recipe_public(self, request, queryset):
+        queryset.update(approved=True)
+
+    def feature_recipe(self, request, queryset):
+        queryset.update(approved=True)
 
 
 @admin.register(Ingredient)
@@ -23,6 +36,10 @@ class IngredientAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'email', 'recipe_title','created_on']
-    list_display = ('name', 'email', 'recipe_title','created_on', 'approved')
-    list_filter = ('name', 'email', 'recipe_title','created_on', 'approved')
+    search_fields = ['name', 'email', 'recipe_title', 'created_on']
+    list_display = ('name', 'email', 'recipe_title', 'created_on', 'approved')
+    list_filter = ('name', 'email', 'recipe_title', 'created_on', 'approved')
+    # actions = ['comment_approved']
+
+    # def comment_approved(self, request, queryset):
+    #     queryset.update(approved=True)
