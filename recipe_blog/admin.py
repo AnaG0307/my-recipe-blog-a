bcf3ab47651029255b2f7db694e0e456c2603f8a
AnaG0307 <ana.genover@gmail.com>
2021-12-03 17:38:46 +0000

    Customised the Admin page by being able to add recipes, comments and ingredient. Also applied filters, the search bar and moified the form to see all the necessary information for all 3 models

--- recipe_blog/admin.py
+++ recipe_blog/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient
+from .models import Recipe, Ingredient, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Recipe, Ingredient)
+@admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
+
+    search_fields = ['author', 'title', 'content', 'created_on', 'approved', 'featured']
+    list_display = ('author', 'title', 'created_on', 'approved', 'featured')
+    date_hierarchy = 'created_on'
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('approved', 'created_on', 'featured')
     summernote_fields = ('content', 'excerpt')
 
 
-# admin.site.register(Ingredients)
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ['ingr_name', 'type']
+    list_display = ('ingr_name', 'type', 'id')
+    list_filter = ('ingr_name', 'type')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'email', 'recipe_title','created_on']
+    list_display = ('name', 'email', 'recipe_title','created_on', 'approved')
+    list_filter = ('name', 'email', 'recipe_title','created_on', 'approved')
