be549c3764973da94ee4129e081c7bdaf32e7169
AnaG0307 <ana.genover@gmail.com>
2021-12-06 14:36:00 +0000

    Downloaded allauth html templates and set up the tags and urls in the base.html file for the user to be able to login, logout or register

--- myrecipebooka/settings.py
+++ myrecipebooka/settings.py
@@ -56,8 +56,8 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-LOGIN_REDIRECT_URL ='/'
-LOGOUT_REDIRECT_URL ='/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
