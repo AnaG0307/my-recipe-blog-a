eb93e5222c8f63fb03ac097a0f0bfb00ae138cbe
AnaG0307 <ana.genover@gmail.com>
2021-12-15 11:43:06 +0000

    Build the admin site related to the users creations

--- user_recipes/admin.py
+++ user_recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserCreation
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(UserCreation)
+class UserCreationAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content', 'ingredients_list')
+    search_fields = ['title', 'content']
+    list_filter = ['title', 'content']
