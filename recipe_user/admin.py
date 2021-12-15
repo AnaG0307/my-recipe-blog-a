9fa638850a6c8b650940342b33adcce104d97e11
AnaG0307 <ana.genover@gmail.com>
2021-12-15 21:34:48 +0000

    Created data model for recipe_user app and built the admin site for it

--- recipe_user/admin.py
+++ recipe_user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserRecipe
 
-# Register your models here.
+
+@admin.register(UserRecipe)
+class UsersRecipes(admin.ModelAdmin):
+    list_display = ('name', 'title', 'created_on')
+    list_filter = ('name', 'created_on')
+    search_fields = ('name', 'email')
