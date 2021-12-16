be8ddad92b8dc55211f9aa664753fb4a8c01c329
AnaG0307 <ana.genover@gmail.com>
2021-12-16 22:02:58 +0000

    Created view code for add_recipe for the user to be able to post their recipes and be able to see them in recipe_list_user.html

--- recipe_blog/forms.py
+++ recipe_blog/forms.py
@@ -6,3 +6,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+
+        widgets = {
+            
+        }
--- recipe_user/views.py
+++ recipe_user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.template.defaultfilters import slugify
 from .models import UserRecipe
 from .forms import UrecipeForm
 
@@ -12,15 +13,28 @@ class UserList(generic.ListView):
     paginate_by = 9
 
 
+def urecipe_detail(request):
+    return render(request, 'recipe_post_user.html')
+
+
 def add_recipe(request):
     submitted = False
     form = UrecipeForm
     if request.method == 'POST':
         form = UrecipeForm(request.POST)
         if form.is_valid():
-            form.instance.name = request.user.username
-            form.save()
+            # form.instance.name = request.user.name
+            new_recipe = form.save(commit=False)
+            print(new_recipe.content)
+            new_recipe.slug = slugify(new_recipe.title)
+            print(new_recipe.slug)
+            # new_recipe.title = slugify(title)
+            new_recipe.save()
             return HttpResponseRedirect('/add_recipe?Submitted=True')
         else:
             form = UrecipeForm()
+            if 'submitted' in request.GET:
+                submitted = True
+                
+
     return render(request, 'recipe_create_user.html', {'form': form, 'submitted': submitted})
